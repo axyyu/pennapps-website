ed5de7334a4d1505f61b86a3710fc6a07b5ca40b
NikiSub <nikileshsub@gmail.com>
2017-09-09 07:59:22 -0400

    started firebase

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
+
 $(document).ready(function(){
     startAni();
     retrieveData();
     openTagline();
 });
 function retrieveData(){
-    retrieveDataAni();
+	
+firebase.initializeApp({databaseURL: "https://debrief-v2.firebaseio.com"});
+var database = firebase.database();
+var topRef = database.ref('topics');
+topRef.on('value', function(snapshot){
+	updateData();
+});
+  
 }
 function openTagline(){
     openTaglineAni();
+}
+function updateData(){
+console.log("changed");
 }
